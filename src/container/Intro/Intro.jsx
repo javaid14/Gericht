906453d0d39cb71c1354812c259c0d1c6f8de0a7
Javaid Ahmad Mir <mirjavaid610@gmail.com>
2023-12-09 22:15:16 +0530

    Adjusted Intro

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -20,26 +20,28 @@ const Intro = () => {
   }
 
   return (
-    <div className='app__video'>  
-      <video
-       src={meal}
-       ref={vidRef}
-       type='video/mp4'
-       loop
-       controls={false}
-       muted
-      />
+    <div className='app__intro'>
+      <div className='app__video'>  
+        <video
+        src={meal}
+        ref={vidRef}
+        type='video/mp4'
+        loop
+        controls={false}
+        muted
+        />
 
-      <div className='app__video-overlay flex__center'>
-        <div 
-          className='app__video-overlay_circle flex__center'
-          onClick={handleVideo}  
-        >
-          {
-            playVideo 
-              ?<BsPauseFill color='#fff' fontSize={30} />
-               : <BsFillPlayFill color='#fff' fontSize={30} /> 
-          }
+        <div className='app__video-overlay flex__center'>
+          <div 
+            className='app__video-overlay_circle flex__center'
+            onClick={handleVideo}  
+          >
+            {
+              playVideo 
+                ?<BsPauseFill color='#fff' fontSize={30} />
+                : <BsFillPlayFill color='#fff' fontSize={30} /> 
+            }
+          </div>
         </div>
       </div>
     </div>
